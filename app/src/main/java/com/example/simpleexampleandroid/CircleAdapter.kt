f1efa894986f7c5b8d47fa546619696bfe0fdab4
ngotuankiet <ngotuankiet@github.com>
2020-08-15 16:13:39 +0700

    rcv

---
+++ app/src/main/java/com/example/simpleexampleandroid/CircleAdapter.kt
@@ -0,0 +1,39 @@
+package com.example.simpleexampleandroid
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+
+class CircleAdapter(private val arrayList: ArrayList<Int>) : RecyclerView.Adapter<CircleAdapter.CircleViewHolder>() {
+
+    inner class CircleViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        private val ivCircle: ImageView = itemView.findViewById(R.id.iv_circle)
+
+
+        //set color
+        fun getColorCircle(position: Int){
+            checkbox.checker = arrayList[position].isdone
+        }
+
+        fun setContentVia{
+            arrayList
+        }
+
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CircleViewHolder {
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.item_circle, parent, false)
+        return CircleViewHolder(v)
+    }
+
+    override fun getItemCount(): Int {
+        return arrayList.size
+    }
+
+    override fun onBindViewHolder(holder: CircleViewHolder, position: Int) {
+        holder.getColorCircle(position)
+    }
+}
--- app/src/main/java/com/example/simpleexampleandroid/MainActivity.kt
+++ app/src/main/java/com/example/simpleexampleandroid/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.simpleexampleandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var rcv: RecyclerView
+    private lateinit var circleAdapter: CircleAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        rcv = findViewById(R.id.rcv_list_circle)
+
+        val arrayList = fakeData()
+        circleAdapter = CircleAdapter(arrayList)
+
+        rcv.apply {
+
+            adapter = circleAdapter
+            layoutManager = LinearLayoutManager(context)
+        }
+    }
+    private fun fakeData(): ArrayList<Int>{
+        val arrayList = ArrayList<Int>()
+        for (i in 0..20){
+            val colorId = 3030 + i
+            arrayList.add(colorId)
+        }
+        return arrayList
     }
 }
