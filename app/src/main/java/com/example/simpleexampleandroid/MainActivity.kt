f3e30579b66e9210f90e6544b3d29fa1272a0166
ngotuankiet <ngotuankiet@github.com>
2020-09-08 11:46:12 +0700

    single selection

--- app/src/main/java/com/example/simpleexampleandroid/MainActivity.kt
+++ app/src/main/java/com/example/simpleexampleandroid/MainActivity.kt
@@ -2,10 +2,44 @@ package com.example.simpleexampleandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.simpleexampleandroid.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var userAdapter1: UserAdapter
+    private lateinit var userAdapter2: UserAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        userAdapter1 = UserAdapter(applicationContext)
+        userAdapter1.submitList(fakeData())
+
+        userAdapter2 = UserAdapter(applicationContext)
+        userAdapter2.submitList(fakeData())
+
+
+        binding.listUser1.apply {
+            adapter = userAdapter1
+            layoutManager = LinearLayoutManager(applicationContext)
+        }
+
+        binding.listUser2.apply {
+            adapter = userAdapter2
+            layoutManager = LinearLayoutManager(applicationContext)
+        }
+
+    }
+
+    fun fakeData(): List<User>{
+        val arrayList = ArrayList<User>()
+        for (i in 0.. 20){
+            val user = User("name $i", i)
+            arrayList.add(user)
+        }
+        return arrayList.toList()
     }
 }
---
+++ app/src/main/java/com/example/simpleexampleandroid/UserAdapter.kt
@@ -0,0 +1,68 @@
+package com.example.simpleexampleandroid
+
+import android.content.Context
+import android.util.Log
+import android.view.ContextMenu
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.simpleexampleandroid.databinding.ItemUserBinding
+import kotlinx.android.synthetic.main.item_user.view.*
+
+class UserAdapter constructor(
+    val context: Context
+) : ListAdapter<User, UserAdapter.UserViewHolder>(UserDiffUtil) {
+
+    // if checkedPosition = -1, there is no default selection
+    // if checkedPosition = 0, 1st item is selected by default
+    private var checkedPosition = -1
+
+    inner class UserViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+
+        private val textView = itemView.findViewById<TextView>(R.id.textViewName)
+        private val imageView = itemView.findViewById<ImageView>(R.id.imageView)
+        fun set(user: User) {
+            textView.text = user.name
+
+            //single item selection
+            if (checkedPosition != adapterPosition) imageView.visibility = View.GONE
+            else if(checkedPosition == adapterPosition) imageView.visibility = View.VISIBLE
+
+            itemView.setOnClickListener {
+                imageView.visibility = View.VISIBLE
+                notifyItemChanged(checkedPosition)
+                checkedPosition = adapterPosition
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_user, parent, false)
+        return UserViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
+        holder.set(getItem(position))
+    }
+
+    object UserDiffUtil : DiffUtil.ItemCallback<User>(){
+        override fun areItemsTheSame(oldItem: User, newItem: User): Boolean {
+            return oldItem.name == newItem.name
+        }
+
+        override fun areContentsTheSame(oldItem: User, newItem: User): Boolean {
+            return oldItem == newItem
+        }
+
+    }
+
+    override fun submitList(list: List<User>?) {
+        super.submitList(list)
+    }
+}
